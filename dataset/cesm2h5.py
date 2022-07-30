d677fc926e2850f173b48097e1c3edbe2c5bf30d
meso272 <liujinyang1201@126.com>
2022-07-29 17:55:50 -0700

    111

--- dataset/cesm2h5.py
+++ dataset/cesm2h5.py
@@ -17,6 +17,7 @@ for subdir in ["HR", "X2"]:
         im_paths = glob.glob(os.path.join(dataset_dir,field,
                                           "hr_train", 
                                           "*.dat"))
+        print(len(im_paths))
 
     else:
         im_paths = glob.glob(os.path.join(dataset_dir,field,
